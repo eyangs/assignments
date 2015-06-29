e06e2ad8fa13e35a154c3d62817ed6557e7afd56
szdbb1102 <2050250960@qq.com>
2015-06-29 12:07:05 +0800

    practice6

--- SRSDEMO/SRSDEMO.UI.Console/model/Section.cs
+++ SRSDEMO/SRSDEMO.UI.Console/model/Section.cs
@@ -273,10 +273,21 @@ public class Section {
     // Student to the AssignedGrades Dictionary.
 
     AssignedGrades.Add(s, te);
-
+  
     return true;
   }
-	
+	//******************************************
+    //创建修改成绩的方法,先删除指定的学生成绩在添加新的成绩
+  public bool EraseGrade(Student s, string grade)
+  {
+      
+      TranscriptEntry te = new TranscriptEntry(s, grade, this);
+      AssignedGrades.Remove(s);
+      AssignedGrades.Add(s, te);
+      return true;
+
+
+  }
   //**************************************
   //
   public bool IsSectionOf(Course c) {
