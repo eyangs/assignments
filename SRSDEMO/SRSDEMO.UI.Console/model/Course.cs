7203143224a0ee98e53cf3a1ee4cb34c710c6299
faruke <1129475412@qq.com>
2015-07-01 17:49:24 +0800

    zuoyesi

--- SRSDEMO/SRSDEMO.UI.Console/model/Course.cs
+++ SRSDEMO/SRSDEMO.UI.Console/model/Course.cs
@@ -98,6 +98,7 @@ public class Course {
 
   //******************************************************************
   //
+   //练习四  在course类定义静态变量SectionNumber，改正逻辑错误
   public Section ScheduleSection(string day, string time, string room,
 				       int capacity) {
     // Create a new Section (note the creative way in
@@ -109,5 +110,10 @@ public class Course {
     OfferedAsSection.Add(s);
 		
     return s;
-  }
+  } 
+   //练习四 增加一个取消section方法 
+    public bool CancelSection(Section s)
+    {
+        this.OfferedAsSection.Remove(s);
+        return true;
 }
